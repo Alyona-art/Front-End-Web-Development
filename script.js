3a826332baa194880a6fcab49d0e381ff1b5569d
Alyona-art <alena.artemeva.2002.15@gmail.com>
2023-02-20 19:44:44 +0300

    updated

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function fetchComic(email) {
     const params = new URLSearchParams([['email', email]]);
     return fetch('https://fwd.innopolis.app/api/hw2?' + params.toString())
         .then((id) => id.text())
-        .then((id) => fetch('https://api.allorigins.win/raw?url=' + 'https://xkcd.com/' + id + '/info.0.json'))
+        .then((id) => fetch('https://getxkcd.vercel.app/api/comic?num=' + id))
         .then((response) => response.json())
         .then((json) => {
             document.getElementById('comic').src = json["img"]
