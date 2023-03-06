c25eacf04f73c38737e95b0a7f5eaa2bbc4c1602
Alyona-art <alena.artemeva.2002.15@gmail.com>
2023-03-06 16:43:03 +0300

    hw3

---
+++ rollup.config.js
@@ -0,0 +1,8 @@
+import typescript from '@rollup/plugin-typescript';
+import terser from '@rollup/plugin-terser';
+
+export default {
+    input: 'src/index.ts',
+    output: [{ file: 'dist/index.min.js', format: 'iife' }],
+    plugins: [typescript(), terser()],
+};
