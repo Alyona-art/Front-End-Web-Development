69c930e0d22c65c05a6e8c78487288d99cd0172f
Alyona-art <alena.artemeva.2002.15@gmail.com>
2023-03-14 10:07:07 +0300

    remove files

--- rollup.config.js
+++
@@ -1,8 +0,0 @@
-import typescript from '@rollup/plugin-typescript';
-import terser from '@rollup/plugin-terser';
-
-export default {
-    input: 'src/index.ts',
-    output: [{ file: 'dist/index.min.js', format: 'iife' }],
-    plugins: [typescript(), terser()],
-};
